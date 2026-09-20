3962b6c7daa2b3ff62238ba19bf35268a151ccd8
inbydev <a80e2517-1246-49a5-907a-c38b8c2c97fb.1yg40@silomails.com>
2026-09-20 01:01:18 -0300

    refactor(backend): move low elementLoadTolerance string to constants dedicated module

--- src/lib/constants.py
+++ src/lib/constants.py
@@ -9,6 +9,9 @@ HARDEN_WEB_STORAGE_JS: Final[str] = (_SCRIPTS_DIR / "HardenWebStorage.js").read_
 AUTH_ERROR_INTERCEPTOR_JS: Final[str] = (_SCRIPTS_DIR / "AuthErrorInterceptor.js").read_text(encoding="utf-8")
 
 IS_A_BUG_STRING: Final[str] = "If you think this is a bug, please go to codeberg.org/Discord-OTP-Forcer/Discord-OTP-Forcer/issues/new and create an issue."
+LOW_ELEMENT_LOAD_TOLERANCE_STRING: Final[str] = (
+    "This may be caused by a low 'elementLoadTolerance' value in your config/program.yml file. Try increasing it to 5 or 7. (Or even higher if your internet connection or computer is slow.)\n If that does not fix the issue, please create a new issue at codeberg.org/Discord-OTP-Forcer/Discord-OTP-Forcer/issues/new to ask for help."
+)
 
 BACKUP_CODE_FIELD: Final[tuple[ByType, str]] = (By.XPATH, "//*[@label='Enter Discord Backup Code']")
 NORMAL_CODE_FIELD: Final[tuple[ByType, str]] = (By.XPATH, "//*[@label='Enter Discord Auth Code']")
